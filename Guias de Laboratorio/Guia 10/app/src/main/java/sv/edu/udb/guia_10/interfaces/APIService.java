175b382de1177e815708aca40862d6b9813c7d82
oscarp3112 <oscarp1998@hotmail.com>
2021-05-03 21:48:13 -0600

    Guia 10 actualizada

---
+++ Guias de Laboratorio/Guia 10/app/src/main/java/sv/edu/udb/guia_10/interfaces/APIService.java
@@ -0,0 +1,33 @@
+package sv.edu.udb.guia_10.interfaces;
+
+import sv.edu.udb.guia_10.models.Producto;
+import sv.edu.udb.guia_10.models.RespProducto;
+import sv.edu.udb.guia_10.models.Respuesta;
+
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Url;
+
+public interface APIService {
+
+    @GET(".")
+    Call<List<Producto>> getProducts();
+
+    @GET()
+    Call<RespProducto> getProductById(@Url String url);
+
+    @POST("agrega")
+    Call<Respuesta> insertProduct(@Body Producto producto);
+
+    @DELETE()
+    Call<Respuesta> deleteProduct(@Url String url);
+
+    @PUT()
+    Call<Respuesta> updateProduct(@Url String url, @Body Producto producto);
+
+}
---
+++ Guias de Laboratorio/Guia 10/app/src/main/java/sv/edu/udb/guia_10/models/Respuesta.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.guia_10.models;
+
+public class Respuesta {
+    private boolean ok;
+    private int resultado;
+
+    public boolean getOk() {
+        return ok;
+    }
+
+    public int getResultado() {
+        return resultado;
+    }
+}
